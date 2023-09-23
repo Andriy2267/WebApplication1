3750f977b7d7635d49fcb7ab53998f469b272de9
Andriy <andrijcopek696@gmail.com>
2023-09-23 23:06:06 +0300

    Add project files.

---
+++ Mango.Services.CouponAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Coupon> Coupons => Set<Coupon>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                Id = 1,
+                CouponCode = "FFF10",
+                CouponAmount = 15,
+                MinAmount = 10
+            });
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                Id = 2,
+                CouponCode = "GG300",
+                CouponAmount = 30,
+                MinAmount = 15
+            });
+        }
+    }
+}
---
+++ Mango.Services.CouponAPI/Migrations/20230923200506_SeedTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Services.CouponAPI.Migrations
+{
+    public partial class SeedTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coupons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CouponCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CouponAmount = table.Column<double>(type: "float", nullable: false),
+                    MinAmount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupons", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "Id", "CouponAmount", "CouponCode", "MinAmount" },
+                values: new object[] { 1, 15.0, "FFF10", 10 });
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "Id", "CouponAmount", "CouponCode", "MinAmount" },
+                values: new object[] { 2, 30.0, "GG300", 15 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupons");
+        }
+    }
+}
