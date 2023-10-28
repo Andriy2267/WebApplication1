43288ef62c1d22ba87a6eee58dda6223803adc3e
Andriy <80958013+Andriy2267@users.noreply.github.com>
2023-10-28 17:12:25 +0300

    Delete Mango.Services.AuthAPI directory

--- Mango.Services.AuthAPI/Dto/LoginRequestDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mango.Services.AuthAPI.Dto
-{
-    public class LoginRequestDto
-    {
-        public string UserName { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
